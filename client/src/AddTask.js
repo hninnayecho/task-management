56e32fbe5691b282928277d86f4064acd0d7afb1
hninnayecho <hninnayecho@gmail.com>
2018-01-06 23:31:41 +0630

    select-input-save

--- client/src/AddTask.js
+++ client/src/AddTask.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-
+import 'react-select/dist/react-select.css';
+import './App.css';
 class AddTask extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+        this.addNewTask = this.addNewTask.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
     addNewTask(e) {
+        var chgValue = this;
         e.preventDefault();
         var task = {
             name: this.name.value,
             dueDate: this.dueDate.value,
-            label: this.label.value,
+            label: chgValue.state.value,
         };
-        this.props.addTask(task);
 
+        this.props.addTask(task);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
     }
 
     render() {
         return (
             <form className="task-form" onSubmit={(e) => this.addNewTask(e)}>
                 <p>Add task</p>
+
                 <input ref={(input) => this.name = input} type="text" placeholder="Task Name" />
+
                 <input ref={(input) => this.dueDate = input} type="text" placeholder="DueDate" />
-                <input ref={(input) => this.label = input} type="text" placeholder="Label" />
+                <div>
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="Pending">Penidng</option>
+                        <option value="Finished">Finished</option>
+                        <option value="InProgress">InProgress</option>
+                    </select></div>
+
                 <button type="submit">Add Task</button>
             </form>
         );
